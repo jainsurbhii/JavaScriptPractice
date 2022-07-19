92dbbe2ef098f291a144c58d7aad52a54584c067
surbhijain <surbhijain.jain4@gmail.com>
2022-07-19 16:37:13 +0530

    July 19 code

---
+++ July19.js
@@ -0,0 +1,24 @@
+/*Ananya loves Surbhi. He sends her gifts every day. But, sometimes when he is busy, he forgets to send gifts for Surbhi.
+
+Given an array of 31 length, where 1 denotes Ananya sent a gift on that day, and 0 denotes Ananya did not send a gift on that day.
+
+Find out the maximum continuous number of days for which Ananya sent a gift to surbhi.
+Example:
+
+[1 1 1 1 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 0]
+Result: 10 Days*/
+
+let arr=[1,1,1,1,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0]
+let count=0;
+let maxvalue=0;
+for(let i=0;i<arr.length;i++){
+    if(arr[i]===1){
+        count++;
+        maxvalue=count;
+    }
+    else {
+        count=0;
+    }
+
+}
+console.log(maxvalue);
