c63975d5cda6d6fbd6296391035ad82a369cf4d5
surbhijain <surbhijain.jain4@gmail.com>
2022-07-28 21:03:59 +0530

    July 27 code modify

--- july27.js
+++ july27.js
@@ -10,17 +10,47 @@ for(let i=str.length-1;i >=0;i--){
 console.log(rev);
 
 */
-let rev=" ";
 
+let rev=" ";
 function revStr(str1) {
-    str1=str1.split(" ")
-    for (let i = str1.length - 1; i >= 0; i--) {
-        rev = rev + str1[i] + " ";
+    //let splitArray=str1.split(x)
+    let splitArray=split(str1)
+    for (let i = splitArray.length - 1; i >= 0; i--) {
+        rev = rev + splitArray[i] + " ";
 
     }
     return rev;
 }
-Stringgg = "I love Surbhi"
+Stringgg = "Ananya loves Surbhi Jain"
 let x= revStr(Stringgg)
 console.log(x);
+
+/*
+* Surbhi Love Ananya
+* return type-Array
+* a="Ananya"
+* arr=["surbhi","Love","Ananya"]
+* */
+
+function split(str5){
+    let arr=[];
+    let str="";
+    let j=0;
+    console.log(str5)
+    for(let i=0;i<str5.length;i++){
+
+        if(str5.charAt(i)===' '){
+            arr[j]=str;
+            j++;
+            str="";
+        }
+        else
+        {
+            str = str + str5.charAt(i);
+        }
+    }
+    arr[j]=str;
+    return arr;
+}
+
 
